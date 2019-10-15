9117e4af1c63012d9eedab3672f968253d3110a7
ryanmao725 <ryan.mao.725@gmail.com>
2019-10-15 11:20:25 -0400

    Added workload A

--- memgrind.c
+++ memgrind.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include<time.h> //Need to include time.h to record times for the workloads
 #include "mymalloc.h"
 
 int main(int argc, char** argv) {
@@ -24,6 +25,25 @@ int main(int argc, char** argv) {
     free(ptr4);
     free(ptr5);
     free(ptr1);
+    
+    struct timeval tv;
+
+    //A
+    //This will malloc() one byte, and then immediately free() it.
+    //Number of Runs: 150
+    int i = 0;
+    gettimeofday(&tv, NULL);
+    time_t start_time = tv.tv_usec;
+    while (i < 150) {
+        char* one_byte = malloc(1);
+        free(one_byte);
+        i++;
+    }
+    gettimeofday(&tv, NULL);
+    time_t end_time = tv.tv_usec;
+
+    printf("The time to execute A was: %ld\n", end_time - start_time);
+
 
     return 0;
 }
