d069e25cb632a3c7c94bb6e56fcee5b482df9a8e
Shivan Modha <shivanmodha@gmail.com>
2019-10-15 00:42:55 -0400

    Some implementation

--- mymalloc.c
+++ mymalloc.c
@@ -1,2 +1,15 @@
-//Actual implementation of the function prototypes declared in the header file "mymalloc.h"
+// Actual implementation of the function prototypes declared in the header file "mymalloc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "mymalloc.h"
 
+int main(int argc, char** argv) {
+    int* k = (int*)malloc(sizeof(int) * 8);
+}
+
+
+static char myblock[4096];
+
+void* mymalloc(size_t size, char* file, int line) {
+    printf("%u", size);
+}
