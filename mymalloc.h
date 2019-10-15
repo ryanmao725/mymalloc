e3164d4d5351093a4d957f3a74123d2f6461d96e
ryanmao725 <ryan.mao.725@gmail.com>
2019-10-15 00:23:17 -0400

    Updated mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -1,5 +1,9 @@
 #ifndef _MYMALLOC_H_
 #define _MYMALLOC_H_
-#define malloc(x) mymalloc(x, __LINE__, __FILE__)
-#define free(x) myfree(x, __LINE__, __FILE__)
+#include<stdlib.h>
+#include<stdio.h>
+#define malloc(x) mymalloc(x, __FILE__, __LINE__)
+#define free(x) myfree(x, __FILE__, __LINE__)
+void* mymalloc(size_t, char*, int);
+void myfree(void*, char*, int);
 #endif
